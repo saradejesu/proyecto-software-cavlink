19e856cc3e040b466b28263074c1e548de52c307
sara rojas <sararojas@MacBook-Pro-de-sara.local>
2026-05-27 09:09:47 -0400

    primer commit

---
+++ back-CAVlink/src/main/java/__71/back_CAVlink/BackCaVlinkApplication.java
@@ -0,0 +1,13 @@
+package __71.back_CAVlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCaVlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCaVlinkApplication.class, args);
+	}
+
+}
---
+++ back-CAVlink/src/test/java/__71/back_CAVlink/BackCaVlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package __71.back_CAVlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCaVlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
